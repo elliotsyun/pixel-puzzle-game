db6ae37690d33abbdbcdbdc3b3eabbd012b6d3c4
dokkaebiyun <elliotyun@vt.edu>
2024-05-21 16:24:08 -0400

    created find_multiples

--- hexgenerator.py
+++ hexgenerator.py
@@ -13,8 +13,19 @@ def generate_hex_value():
     return color
 
 
+def find_multiples(number, m):
+    '''Prints the first m multiples of a number'''
+    mul_list = []
+
+    for i in range(0, m*number, number):
+        mul_list.add(i)
+
+    return mul_list
+
+
 def generate_next_value():
     '''Generates the next hex value to compare it with'''
+
 
     # bottom and top should be multiples away from the other one
     other_color = generate_hex_value()
