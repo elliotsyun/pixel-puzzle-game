839e9d333b4eef39bfbd9e61aa88fbcc1fc46a41
Michael <dunn0562@umn.edu>
2024-05-21 14:52:40 -0500

    main

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import pygame
+
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
