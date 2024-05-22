567d02f8c85f7d56c5860d2d7be77d7a810c8db3
dokkaebiyun <elliotyun@vt.edu>
2024-05-22 17:25:24 -0400

    created config file with global const and window.py was created to separate main.py up

---
+++ config.py
@@ -0,0 +1,5 @@
+import pygame
+
+WIDTH, HEIGHT = 400, 400
+GAME_NAME = "COLOR'D"
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
--- main.py
+++ main.py
@@ -1,15 +1,5 @@
 import pygame
-
-WIDTH, HEIGHT = 400, 400
-GAME_NAME = "COLOR'D"
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-
-
-BUTTON_SCALE = (120, 120)
-D_PLUS_IMG = pygame.image.load("./Assets/PLUSPLUS.png")
-PLUS_IMG = pygame.image.load("./Assets/PLUS.png")
-D_MIN_IMG = pygame.image.load("./Assets/MINMIN.png")
-MIN_IMG = pygame.image.load("./Assets/MIN.png")
+from config import WIDTH, HEIGHT, GAME_NAME, WIN
 
 
 def main():
@@ -26,31 +16,12 @@ def main():
                 color = WIN.get_at(coord)
                 print("Color: ", color)
 
-        white_to_black = pygame.Surface((1, 2))
-        white_to_black.fill((255, 255, 255))
-        white_to_black.set_at((0, 1), (0, 0, 0))
-        white_to_black = pygame.transform.smoothscale(white_to_black, (255, 255))
-
-        white_to_color = pygame.Surface((2, 1))
-        white_to_color.fill((255, 255, 255))
-        
-        changing_color = (255, 0, 0)
-        # below code is changing the color, to red which is (255, 0, 0)
-        white_to_color.set_at((1, 0), changing_color)
-        white_to_color = pygame.transform.smoothscale(white_to_color, (255, 255))
-        white_to_black.blit(white_to_color, (0, 0), special_flags=pygame.BLEND_MULT)
-
-        WIN.blit(white_to_black, (75, 75))
-
-        pygame.display.flip()
-        pygame.event.get()
-
         pygame.display.update()
 
     pygame.quit()
 
 
-def window():
+def initialize_window():
     """window initialization"""
     WIN.fill((255, 255, 255))
     pygame.display.set_caption(GAME_NAME)
