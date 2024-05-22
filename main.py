786612ecf57d92c7a0ab6e3267c75e306ad23e2b
Michael <dunn0562@umn.edu>
2024-05-22 14:34:53 -0500

    buttons and circle

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pygame
+import hexgenerator as hg
 
 WIDTH, HEIGHT = 600, 500
 GAME_NAME = "COLOR'D"
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 BUTTON_SCALE = (120, 120)
+COLOR_SCALE = (120, 120)
 D_PLUS_IMG = pygame.image.load('./Assets/PLUSPLUS.png')
 PLUS_IMG = pygame.image.load('./Assets/PLUS.png')
 D_MIN_IMG = pygame.image.load('./Assets/MINMIN.png')
 MIN_IMG = pygame.image.load('./Assets/MIN.png')
+WHITE = (255, 255, 255)
 
 
 def main():
@@ -18,6 +21,7 @@ def main():
                 run = False
         window()
         button()
+        color_circ()
         pygame.display.update()
 
     pygame.quit()
@@ -35,11 +39,36 @@ def button():
     plus_img_s = pygame.transform.scale(PLUS_IMG, BUTTON_SCALE)
     d_min_img_s = pygame.transform.scale(D_MIN_IMG, BUTTON_SCALE)
     min_img_s = pygame.transform.scale(MIN_IMG, BUTTON_SCALE)
+    new = min_img_s.fill((255, 0, 0, 100))
     WIN.blit(d_plus_img_s, (450, 300))
     WIN.blit(plus_img_s, (315, 300))
     WIN.blit(d_min_img_s, (25, 300))
     WIN.blit(min_img_s, (165, 300))
 
+
+def color_circ():
+    '''creates the randomized color circles'''
+    rand_color = (0, 255, 0)
+    user_color = (0, 0, 255)
+
+    circ_radius = 10
+    surf = pygame.Surface((100, 100))
+
+
+    CIRCLE1 = pygame.Surface((100, 100))
+    CIRCLE1.fill((255, 0, 0))
+    pygame.draw.rect(CIRCLE1, rand_color, ((10, 10), (150, 150)), width=0)
+
+    CIRCLE1 = pygame.Surface((100, 100))
+    CIRCLE1.fill(WHITE)
+    pygame.draw.circle(CIRCLE1, rand_color, (50, 50), 20, width=0)
+    WIN.blit(CIRCLE1, (200, 200))
+
+
+    CIRCLE2 = pygame.Surface((100, 100))
+    CIRCLE2.fill(WHITE)
+    pygame.draw.circle(CIRCLE2, user_color, (150, 150), 10, width=0)
+    WIN.blit(CIRCLE2, (100, 100))
 
 
 if __name__ == "__main__":
