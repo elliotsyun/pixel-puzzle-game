aaca2bb3835cb86db3ad7f601a7eb8969a3d206f
dokkaebiyun <elliotyun@vt.edu>
2024-05-22 15:40:50 -0400

    Michael's main added

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import pygame
 
-WIDTH, HEIGHT = 900, 500
+WIDTH, HEIGHT = 600, 500
+GAME_NAME = "COLOR'D"
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+BUTTON_SCALE = (120, 120)
+D_PLUS_IMG = pygame.image.load('./Assets/PLUSPLUS.png')
+PLUS_IMG = pygame.image.load('./Assets/PLUS.png')
+D_MIN_IMG = pygame.image.load('./Assets/MINMIN.png')
+MIN_IMG = pygame.image.load('./Assets/MIN.png')
 
 
 def main():
@@ -10,5 +16,31 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        window()
+        button()
+        pygame.display.update()
 
     pygame.quit()
+
+
+def window():
+    '''window initialization'''
+    WIN.fill((255, 255, 255))
+    pygame.display.set_caption(GAME_NAME)
+
+
+def button():
+    '''button scaling, sizing and placement on the window'''
+    d_plus_img_s = pygame.transform.scale(D_PLUS_IMG, BUTTON_SCALE)
+    plus_img_s = pygame.transform.scale(PLUS_IMG, BUTTON_SCALE)
+    d_min_img_s = pygame.transform.scale(D_MIN_IMG, BUTTON_SCALE)
+    min_img_s = pygame.transform.scale(MIN_IMG, BUTTON_SCALE)
+    WIN.blit(d_plus_img_s, (450, 300))
+    WIN.blit(plus_img_s, (315, 300))
+    WIN.blit(d_min_img_s, (25, 300))
+    WIN.blit(min_img_s, (165, 300))
+
+
+
+if __name__ == "__main__":
+    main()
